22019d90c7002ab39cb542513587dd78f9aaf940
Afador <200121874+Afad0r@users.noreply.github.com>
2025-10-24 13:32:36 +0100

    - Changed Devices.java to Usb.java - Changed methods within Usb.java

---
+++ src/main/java/codenameburkley/Usb.java
@@ -0,0 +1,70 @@
+package codenameburkley;
+
+import oshi.SystemInfo;
+import oshi.hardware.UsbDevice;
+
+import java.util.List;
+
+public class Usb {
+    private final SystemInfo si = new SystemInfo();
+    private List<UsbDevice> usbDevices;
+
+    public String[] getNames() {
+        String[] names = new String[usbDevices.size()];
+        int i = 0;
+
+        for (UsbDevice usb: usbDevices) {
+            names[i] = usb.getName();
+            i++;
+        }
+        return names;
+    }
+
+    public String[] getProductIds() {
+        String[] productIds = new String[usbDevices.size()];
+        int i = 0;
+
+        for (UsbDevice usb: usbDevices) {
+            productIds[i] = usb.getProductId();
+            i++;
+        }
+        return productIds;
+    }
+
+    public String[] getSerialNumbers() {
+        String[] serialNumbers = new String[usbDevices.size()];
+        int i = 0;
+
+        for (UsbDevice usb: usbDevices) {
+            serialNumbers[i] = usb.getName();
+            i++;
+        }
+        return serialNumbers;
+    }
+
+    public String[] getVendors() {
+        String[] vendors = new String[usbDevices.size()];
+        int i = 0;
+
+        for (UsbDevice usb: usbDevices) {
+            vendors[i] = usb.getName();
+            i++;
+        }
+        return vendors;
+    }
+
+    public String[] getVendorsId() {
+        String[] vendorsId = new String[usbDevices.size()];
+        int i = 0;
+
+        for (UsbDevice usb: usbDevices) {
+            vendorsId[i] = usb.getName();
+            i++;
+        }
+        return vendorsId;
+    }
+
+    private void refreshUsbList() {
+        usbDevices = si.getHardware().getUsbDevices(false);
+    }
+}

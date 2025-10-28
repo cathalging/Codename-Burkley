7ff2992a2cdf501c4491c267c9bc15cb0e5ca9e0
Afador <200121874+Afad0r@users.noreply.github.com>
2025-10-28 10:11:36 +0000

    - Added NetworkCard.java methods

--- src/main/java/codenameburkley/Controller.java
+++ src/main/java/codenameburkley/Controller.java
@@ -8,6 +8,7 @@ public class Controller {
     GraphicsCards gpus = new GraphicsCards();
     Usb usbs = new Usb();
     CPU cpu = new CPU();
+    NetworkCard networkCard = new NetworkCard(true);
 
     //GPU Info
     public void getGraphicsInfo() {
@@ -64,5 +65,38 @@ public class Controller {
     //CPU Info
     public void getCPUInfo() {
         
+    }
+
+    //Network Info
+    public void getNetworkInfo() {
+        System.out.println("\nNames:");
+        for (String name : networkCard.getNames()) {
+            System.out.println(name);
+        }
+
+        System.out.println("\nDisplay Names:");
+        for (String displayName : networkCard.getDisplayNames()) {
+            System.out.println(displayName);
+        }
+
+        System.out.println("\nMac Addresses:");
+        for (String macAddress : networkCard.getMacaddrs()) {
+            System.out.println(macAddress);
+        }
+
+        System.out.println("\nIPv4 Addresses:");
+        for (String[] ipv4Address : networkCard.getIPv4addrs()) {
+            System.out.println(ipv4Address);
+        }
+
+        System.out.println("\nIPv6 Addresses:");
+        for (String[] ipv6Address : networkCard.getIPv6addr()) {
+            System.out.println(ipv6Address);
+        }
+
+        System.out.println("\nSpeeds:");
+        for (long speed : networkCard.getSpeeds()) {
+            System.out.println(speed);
+        }
     }
 }
